9fdf2716d84a0b6990064d70540351693aa02ac3
DanielCanaris <danielcanaris@yahoo.com>
2022-02-28 06:08:22 -0600

    initial commit

--- server.js
+++ server.js
@@ -1,18 +1,24 @@
-import express from 'express';
-import dotenv from 'dotenv';
+const express = require('express');
+const dotenv = require('dotenv');
+const path = require('path');
 
 const app = express();
 dotenv.config();
 
+const port = process.env.PORT || 5000;
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hjs');
+
 app.get('/', (req, res, next) => {
-	return res.send('works this is a clone');
+	return res.render('home');
 });
 
 app.use((err, req, res, next) => {
 	return res.send(`Error: ${err}`);
 });
 
-app.listen(process.env.PORT, (err) => {
-	if (err) return console.log(`Error hosting on port ${process.env.PORT}.  Error: ${err}`);
-	return console.log(`Now hosting on port ${process.env.PORT}.`);
+app.listen(port, (err) => {
+	if (err) return console.log(`Error hosting on port ${port}.  Error: ${err}`);
+	return console.log(`Now hosting on port ${port}.`);
 });
